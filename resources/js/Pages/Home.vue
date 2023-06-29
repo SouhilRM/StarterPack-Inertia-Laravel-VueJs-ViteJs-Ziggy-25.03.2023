b0824bbd5fa756dbc1740d771269590f48dc839e
SouhilRM <r.m.souhil47@gmail.com>
2023-06-29 23:55:38 +0100

    StarterPack

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>
+            HOME NUMBER 111
+        </h1>
+    </div>
+    <div>
+        <Link :href="route('route_2')">
+            vers home 222
+        </Link>
+    </div>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+</script>
---
+++ resources/js/Pages/Home2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>
+            HOME NUMBER 222
+        </h1>
+    </div>
+    <div>
+        <Link :href="route('route_1')">
+            vers home 111
+        </Link>
+    </div>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+</script>
